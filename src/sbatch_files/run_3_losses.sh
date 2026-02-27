67801c79d14b32f67f0c52b45147188e2d0e72eb
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-02-27 10:15:06 +0100

    try sbatch files

---
+++ src/sbatch_files/run_3_losses.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=kinematic_training
+#SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/src/logs/output_%j.log
+#SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/src/logs/error_%j.log
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:a40:1
+
+mkdir -p /home/nfs/agrigore/projects/githubProjects/master-thesis/src/logs
+
+cd /home/nfs/agrigore/projects/githubProjects/master-thesis/src
+
+apptainer exec --nv \
+    --bind /tudelft.net:/tudelft.net \
+    /tudelft.net/staff-umbrella/StudentsCVlab/agrigore/artist-local.sif \
+    python kinematic_reconstruction_training.py
