2f6519bea294f1c5aa06a4cda87cc293b1863f56
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-02-27 14:27:16 +0100

    rename from kl to kr

--- src/sbatch_files/run_normal_kr_train.sh
+++ src/sbatch_files/run_normal_kr_train.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=kinematic_training
 #SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/output_%j.log
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/error_%j.log
-#SBATCH --time=1:00:00
+#SBATCH --time=0:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
@@ -14,4 +14,4 @@ cd /home/nfs/agrigore/projects/githubProjects/master-thesis/src
 apptainer exec --nv \
     --bind /tudelft.net:/tudelft.net \
     /tudelft.net/staff-umbrella/StudentsCVlab/agrigore/artist-local.sif \
-    python kl_train_3_losses/main.py
+    python normal_kr_training/main.py
