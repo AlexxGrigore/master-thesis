1f52e9e560af9f7bb3b1657c0651c36c9291351e
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-02-27 12:27:43 +0100

    fix log paths

--- src/sbatch_files/run_normal_kl_train.sh
+++ src/sbatch_files/run_normal_kl_train.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=kinematic_training
-#SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/src/logs/output_%j.log
-#SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/src/logs/error_%j.log
+#SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/output_%j.log
+#SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/error_%j.log
 #SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
 
-mkdir -p /home/nfs/agrigore/projects/githubProjects/master-thesis/src/logs
+mkdir -p /home/nfs/agrigore/projects/githubProjects/master-thesis/logs
 
 cd /home/nfs/agrigore/projects/githubProjects/master-thesis/src
 
 apptainer exec --nv \
     --bind /tudelft.net:/tudelft.net \
     /tudelft.net/staff-umbrella/StudentsCVlab/agrigore/artist-local.sif \
-    python kinematic_reconstruction_training/main.py
+    python normal_kl_training/main.py
