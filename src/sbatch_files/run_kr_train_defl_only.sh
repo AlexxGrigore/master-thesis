3e062db3739611e8d4e42e92d43a77e033afa1c2
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-02-28 16:26:11 +0100

    update mem req

--- src/sbatch_files/run_kr_train_defl_only.sh
+++ src/sbatch_files/run_kr_train_defl_only.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/error_%j.log
 #SBATCH --time=0:30:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --mem=8G
 #SBATCH --gres=gpu:a40:1
 
 mkdir -p /home/nfs/agrigore/projects/githubProjects/master-thesis/logs
