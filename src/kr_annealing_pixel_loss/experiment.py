6ed76ccd4918c1fe9158ec92ec1a4ea2fea73bfb
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-03-11 10:47:08 +0100

    reduce rays

--- src/kr_annealing_pixel_loss/experiment.py
+++ src/kr_annealing_pixel_loss/experiment.py
@@ -66,8 +66,8 @@ def run_experiment(
                 number_of_surface_points_per_facet=torch.tensor([25, 25]),
             )
 
-        scenario.set_number_of_rays(20)
-        log.info("Number of rays set to 20.")
+        scenario.set_number_of_rays(10)
+        log.info("Number of rays set to 10.")
 
         print(f"  Heliostats: {scenario.heliostat_field.number_of_heliostats_per_group.sum().item()}")
 
