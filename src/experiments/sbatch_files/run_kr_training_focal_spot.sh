325b6b9b64aaede13ab83ad613479add922cf846
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-04-18 12:56:42 +0200

    increase time

--- src/experiments/sbatch_files/run_kr_training_focal_spot.sh
+++ src/experiments/sbatch_files/run_kr_training_focal_spot.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=focal_spot
 #SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/focal_spot_out_%j.log
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/focal_spot_err_%j.log
-#SBATCH --time=1:32:30
+#SBATCH --time=2:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
--- src/experiments/sbatch_files/run_pixel_loss_2_stages_with_blurr.sh
+++ src/experiments/sbatch_files/run_pixel_loss_2_stages_with_blurr.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=pixel_blurr
 #SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/pixel_blurr_out_%j.log
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/pixel_blurr_err_%j.log
-#SBATCH --time=2:12:33
+#SBATCH --time=3:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
