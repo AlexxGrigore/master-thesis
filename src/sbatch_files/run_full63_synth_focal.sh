c08639ebd09f677944ee2d1ef56414e341d5aef0
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-05-19 23:31:11 +0200

    update timing sbatch

--- src/sbatch_files/run_full63_synth_focal.sh
+++ src/sbatch_files/run_full63_synth_focal.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=f63_synth_focal
 #SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/f63_synth_focal_out_%j.log
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/f63_synth_focal_err_%j.log
-#SBATCH --time=2:00:00
+#SBATCH --time=00:45:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
