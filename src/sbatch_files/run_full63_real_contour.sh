d713b219c75ad9f623418730f0a375b992e5adcb
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-05-25 17:37:22 +0200

    Update run_full63_real_contour.sh

--- src/sbatch_files/run_full63_real_contour.sh
+++ src/sbatch_files/run_full63_real_contour.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=f63_real_contour
 #SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/f63_real_contour_out_%j.log
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/f63_real_contour_err_%j.log
-#SBATCH --time=03:00:00
+#SBATCH --time=01:30:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
