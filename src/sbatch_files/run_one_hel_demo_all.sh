b69aa06d8cefa9e448fd249d80d2ad9064170785
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2026-06-02 20:30:29 +0200

    Update run_one_hel_demo_all.sh

--- src/sbatch_files/run_one_hel_demo_all.sh
+++ src/sbatch_files/run_one_hel_demo_all.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=one_hel_demo_all
 #SBATCH --output=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/one_hel_demo_all_out_%j.log
 #SBATCH --error=/home/nfs/agrigore/projects/githubProjects/master-thesis/logs/one_hel_demo_all_err_%j.log
-#SBATCH --time=02:45:00
+#SBATCH --time=01:45:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:a40:1
